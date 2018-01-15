e9507189307c052215bfcc09e188bbb69a80872d
kazu7391 <vinhtq.0703@gmail.com>
2018-01-15 13:58:58 +0700

    Diva Featured Categories

---
+++ Modules/OpenCart/Diva Featured Categories/admin/language/en-gb/extension/module/dvfeaturedcate.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']    = '<b><i>DIVA Featured Categories</i></b>';
+$_['page_title']    = 'DIVA Featured Categories';
+
+// Tabs
+$_['tab_show_cate']     = "Categories Settings";
+$_['tab_show_products'] = "Products Slider Settings";
+
+// Text
+$_['text_extension']      = 'Extensions';
+$_['text_success']     = 'Success: You have modified module!';
+$_['text_edit']        = 'DIVA Featured Categories Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_slider']       = 'Use Slider';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
+$_['entry_item']      = 'Items';
+$_['entry_limit']      = 'Limit';
+$_['entry_speed']      = 'Speed';
+$_['entry_autoplay']     = 'Auto Play';
+$_['entry_shownextback']     = 'Show Next/Back';
+$_['entry_showdes']     = 'Show Description';
+$_['entry_showsub']     = 'Show Sub Categories';
+$_['entry_showsubnumber']     = 'Number of Sub Categories';
+$_['entry_shownav']     = 'Show Navigation';
+$_['entry_rows']     = 'Rows of Categories';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify this module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
