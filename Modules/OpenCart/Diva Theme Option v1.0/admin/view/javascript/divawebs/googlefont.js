bbb393b50559093b2bb2a8a7da53916979e233d1
kazu7391 <vinhtq.0703@gmail.com>
2018-01-28 23:11:30 +0700

    Theme Option

--- Modules/OpenCart/Diva Theme Option v1.0/admin/view/javascript/divawebs/googlefont.js
+++ Modules/OpenCart/Diva Theme Option v1.0/admin/view/javascript/divawebs/googlefont.js
@@ -32,27 +32,80 @@ var gfont = {
         var subsets = fonts[font_id]['subsets'];
 
         if (variants.length) {
+            var length = variants.length;
+            var divide_count = 7;
+
             variants_html += "<label class='control-label'>" + text_font_weight + "</label>";
-            variants_html += "<div class='lbl-checkbox'>";
-            for (i = 0; i < variants.length; i++) {
-                variants_html += "<label class='cbk-container'>" + variants[i];
-                variants_html += "<input type='checkbox' value='" + variants[i] + "'>";
-                variants_html += "<span class='checkmark'></span>";
-                variants_html += "</label>";
+            variants_html += "<div class='lbl-checkbox row'>";
+            if (length <= divide_count) {
+                variants_html += "<div class='col-sm-6'>";
+                for (i = 0; i < variants.length; i++) {
+                    variants_html += "<label class='cbk-container'>" + variants[i];
+                    variants_html += "<input type='checkbox' value='" + variants[i] + "'>";
+                    variants_html += "<span class='checkmark'></span>";
+                    variants_html += "</label>";
+                }
+                variants_html += "</div>";
             }
+
+            if (length > divide_count) {
+                variants_html += "<div class='col-sm-6'>";
+                for (i = 0; i < divide_count; i++) {
+                    variants_html += "<label class='cbk-container'>" + variants[i];
+                    variants_html += "<input type='checkbox' value='" + variants[i] + "'>";
+                    variants_html += "<span class='checkmark'></span>";
+                    variants_html += "</label>";
+                }
+                variants_html += "</div>";
+                variants_html += "<div class='col-sm-6'>";
+                for (i = 7; i < length; i++) {
+                    variants_html += "<label class='cbk-container'>" + variants[i];
+                    variants_html += "<input type='checkbox' value='" + variants[i] + "'>";
+                    variants_html += "<span class='checkmark'></span>";
+                    variants_html += "</label>";
+                }
+                variants_html += "</div>";
+            }
+
             variants_html += "</div>";
         }
 
         selection.closest('.font-control').find('.font-variant-ckb').html(variants_html);
 
         if (subsets.length) {
+            var length = subsets.length;
+            var divide_count = 7;
+
             subsets_html += "<label class='control-label'>" + text_font_subset + "</label>";
-            subsets_html += "<div class='lbl-checkbox'>";
-            for (i = 0; i < subsets.length; i++) {
-                subsets_html += "<label class='cbk-container'>" + subsets[i];
-                subsets_html += "<input type='checkbox' value='" + subsets[i] + "'>";
-                subsets_html += "<span class='checkmark'></span>";
-                subsets_html += "</label>";
+            subsets_html += "<div class='lbl-checkbox row'>";
+            if (length <= divide_count) {
+                subsets_html += "<div class='col-sm-6'>";
+                for (i = 0; i < subsets.length; i++) {
+                    subsets_html += "<label class='cbk-container'>" + subsets[i];
+                    subsets_html += "<input type='checkbox' value='" + subsets[i] + "'>";
+                    subsets_html += "<span class='checkmark'></span>";
+                    subsets_html += "</label>";
+                }
+                subsets_html += "</div>";
+            }
+
+            if (length > divide_count) {
+                subsets_html += "<div class='col-sm-6'>";
+                for (i = 0; i < divide_count; i++) {
+                    subsets_html += "<label class='cbk-container'>" + subsets[i];
+                    subsets_html += "<input type='checkbox' value='" + subsets[i] + "'>";
+                    subsets_html += "<span class='checkmark'></span>";
+                    subsets_html += "</label>";
+                }
+                subsets_html += "</div>";
+                subsets_html += "<div class='col-sm-6'>";
+                for (i = 7; i < length; i++) {
+                    subsets_html += "<label class='cbk-container'>" + subsets[i];
+                    subsets_html += "<input type='checkbox' value='" + subsets[i] + "'>";
+                    subsets_html += "<span class='checkmark'></span>";
+                    subsets_html += "</label>";
+                }
+                subsets_html += "</div>";
             }
             subsets_html += "</div>";
         }
