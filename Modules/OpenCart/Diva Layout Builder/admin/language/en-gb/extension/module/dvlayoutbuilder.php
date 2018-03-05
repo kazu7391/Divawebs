e0c0766b8969bc972adef838e961c69375d1cdaa
kazu7391 <vinhtq.0703@gmail.com>
2018-03-05 23:17:39 +0700

    Layout Builder

---
+++ Modules/OpenCart/Diva Layout Builder/admin/language/en-gb/extension/module/dvlayoutbuilder.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']         = '<b style="color: #ff7a05;"><i><i class="fa fa-cubes"></i> DIVA Layout Builder</i></b>';
+$_['heading_primary_title'] = '<b style="color: #ff7a05;"><i><i class="fa fa-cubes"></i> DIVA Layout Builder</i></b>';
+$_['page_title']            = "DIVA Layout Builder";
+
+// Text
+$_['text_success']          = "Success: You have modified DIVA Layout Builder module!";
+$_['text_extension']        = 'Extensions';
+$_['text_primary_edit']     = "Edit DIVA Layout Builder";
+$_['text_columns']          = "Columns";
+$_['text_custom_columns']   = "Columns Customization";
+$_['text_custom_classname'] = "Custom Classname";
+$_['text_columns_error_format'] = "Can't create columns because of columns format";
+$_['text_add_row']          = "Add New Row";
+$_['text_add_sub_row']      = "Add Row in this Column";
+$_['text_add_module']       = "Insert modules";
+$_['text_insert_module']    = "Insert or Drag modules here";
+$_['text_number_max_over']  = "The number of columns must be less than 12";
+$_['text_number_min_over']  = "The number of columns must be more than 0";
+
+// Entry
+$_['entry_name']        = "Content Name";
+$_['entry_status']      = "Status";
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify DIVA Layout Builder module!';
+$_['error_name']        = 'Content Name must be between 3 and 64 characters!';
