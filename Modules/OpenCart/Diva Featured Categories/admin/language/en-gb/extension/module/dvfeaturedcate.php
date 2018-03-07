a6d4e5414fb9bec36832cec7c0bd04ffb62b91c0
kazu7391 <vinhtq.0703@gmail.com>
2018-03-07 16:24:44 +0700

    Featured Category

--- Modules/OpenCart/Diva Featured Categories/admin/language/en-gb/extension/module/dvfeaturedcate.php
+++ Modules/OpenCart/Diva Featured Categories/admin/language/en-gb/extension/module/dvfeaturedcate.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']         = '<b style="color: #d21f27;"><i><i class="fa fa-tag"></i> DIVA Featured Categories</i></b>';
+$_['heading_title']         = '<b style="color: #ff7a05;"><i><i class="fa fa-tag"></i> DIVA Featured Categories</i></b>';
 $_['page_title']            = 'DIVA Featured Categories';
 
 // Tabs
