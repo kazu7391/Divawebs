0b98ec78185bbeace72814acc5593aae20ce5310
kazu7391 <vinhtq.0703@gmail.com>
2018-03-15 23:33:22 +0700

    Static Block

--- Modules/OpenCart/Diva Static Block/admin/language/en-gb/extension/module/dvstaticblock.php
+++ Modules/OpenCart/Diva Static Block/admin/language/en-gb/extension/module/dvstaticblock.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: My PC
- * Date: 15/03/2018
- * Time: 4:44 CH
- */
+// Heading
+$_['heading_title']     = '<b style="color: #eb5202;"><i><i class="fa fa-object-group"></i> DIVA Static Block</i></b>';
+$_['page_title']        = 'DIVA Static Block';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified module!';
+$_['text_edit']         = 'Edit DIVA Static Block Module';
+
+// Entry
+$_['entry_name']        = 'Module Name';
+$_['entry_title']       = 'Heading Title';
+$_['entry_description'] = 'Description';
+$_['entry_status']      = 'Status';
+$_['entry_use_title']   = 'Show Title';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify module!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
