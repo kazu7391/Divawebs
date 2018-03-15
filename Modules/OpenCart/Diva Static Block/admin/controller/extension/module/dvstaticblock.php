bbdf41bab5d13558143fdc8141efd6b482586b20
kazu7391 <vinhtq.0703@gmail.com>
2018-03-15 16:49:14 +0700

    Static Block

---
+++ Modules/OpenCart/Diva Static Block/admin/controller/extension/module/dvstaticblock.php
@@ -0,0 +1,5 @@
+<?php
+class ControllerExtensionModuleDvstaticblock extends Controller
+{
+    private $error = array();
+}
---
+++ Modules/OpenCart/Diva Static Block/admin/language/en-gb/extension/module/dvstaticblock.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: My PC
+ * Date: 15/03/2018
+ * Time: 4:44 CH
+ */
