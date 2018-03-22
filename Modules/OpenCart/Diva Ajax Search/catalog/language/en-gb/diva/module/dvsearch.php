8b0ee2755c447cb6a325f73d345d7b4bab2dd1f5
kazu7391 <vinhtq.0703@gmail.com>
2018-03-22 15:56:29 +0700

    Ajax Search

--- Modules/OpenCart/Diva Ajax Search/catalog/language/en-gb/diva/module/dvsearch.php
+++ Modules/OpenCart/Diva Ajax Search/catalog/language/en-gb/diva/module/dvsearch.php
@@ -1,6 +1,5 @@
 <?php
 // Text
-$_['text_search'] = 'Search';
+$_['text_search']       = 'Search';
 $_['text_category']     = 'All Categories';
 $_['text_empty']        = 'There is no product that matches the search criteria.';
-$_['text_tax']        = 'Ex Tax:';
