e0f2bfc660a42b1d9eb6ecfdd510a2e994851c60
kazu7391 <vinhtq.0703@gmail.com>
2018-03-29 10:55:19 +0700

    Theme Option

--- Modules/OpenCart/Diva Theme Option v1.0/admin/language/en-gb/diva/adminmenu.php
+++ Modules/OpenCart/Diva Theme Option v1.0/admin/language/en-gb/diva/adminmenu.php
@@ -7,5 +7,5 @@ $_['text_blog'] = "Blog";
 $_['text_article'] = "Articles";
 $_['text_article_list'] = "Articles Lists";
 $_['text_blog_config'] = "Configuration";
-$_['text_testimonial'] = "Testimonials";
+$_['text_testimonial'] = "Testimonial";
 $_['text_slideshow'] = "Sliders Management";
