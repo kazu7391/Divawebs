29441d3fb1f9984bab3c822e76064a6e1ba15c18
kazu7391 <vinhtq.0703@gmail.com>
2018-04-01 10:16:21 +0700

    Newsletter

---
+++ Modules/OpenCart/Diva Newsletter/admin/controller/extension/module/dvnewsletter.php
@@ -0,0 +1,5 @@
+<?php
+class ControllerExtensionModuleDvnewsletter extends Controller
+{
+
+}
---
+++ Modules/OpenCart/Diva Newsletter/admin/language/en-gb/extension/module/dvnewsletter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HOC TAP
+ * Date: 01-Apr-18
+ * Time: 10:10 AM
+ */
