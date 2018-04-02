76ccc859e1695241b0b609d35e62ca1ea6944fc5
kazu7391 <vinhtq.0703@gmail.com>
2018-04-02 16:22:30 +0700

    Newsletter

--- Modules/OpenCart/Diva Newsletter/admin/language/en-gb/extension/module/dvnewsletter.php
+++ Modules/OpenCart/Diva Newsletter/admin/language/en-gb/extension/module/dvnewsletter.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: HOC TAP
- * Date: 01-Apr-18
- * Time: 10:10 AM
- */
+// Heading
+$_['heading_title']    		= '<b style="color: #eb5202;"><i><i class="fa fa-envelope"></i> DIVA Newsletter</i></b>';
+$_['page_title']    		= 'DIVA Newsletter';
+
+// Text
+$_['text_extension']        = 'Extensions';
+$_['text_edit']             = 'Edit DIVA Newsletter module';
+$_['text_success']        	= 'Success: You have modified module DIVA Newsletter!';
+$_['text_default']         	= 'Default';
+
+// Entry
+$_['entry_status']       	= 'Status';
+$_['entry_name']        	= 'Module Name';
+$_['entry_popup']        	= 'Use Popup';
+
+// Error
+$_['error_permission'] 		= 'Warning: You do not have permission to modify this module!';
+$_['error_name']            = 'Module Name must be between 3 and 64 characters!';
--- Modules/OpenCart/Diva Newsletter/admin/model/diva/newsletter.php
+++ Modules/OpenCart/Diva Newsletter/admin/model/diva/newsletter.php
@@ -1,5 +1,25 @@
 <?php
 class ModelDivaNewsletter extends Model
 {
+    public function install() {
+        $this->db->query("
+			CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "dvnewsletter_email` (
+			    `newsletter_id` INT(11) NOT NULL AUTO_INCREMENT,
+			    `subscribe` TINYINT(1) NOT NULL DEFAULT '1',
+	            `mail` varchar(255) NOT NULL,
+	        PRIMARY KEY (`newsletter_id`)
+		) DEFAULT COLLATE=utf8_general_ci;");
 
+        $this->load->model('user/user_group');
+        $this->model_user_user_group->addPermission($this->user->getGroupId(), 'access', 'diva/newsletter');
+        $this->model_user_user_group->addPermission($this->user->getGroupId(), 'modify', 'diva/newsletter');
+    }
+
+    public function uninstall() {
+        $this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "dvnewsletter_email`");
+
+        $this->load->model('user/user_group');
+        $this->model_user_user_group->removePermission($this->user->getGroupId(), 'access', 'diva/newsletter');
+        $this->model_user_user_group->removePermission($this->user->getGroupId(), 'modify', 'diva/newsletter');
+    }
 }
