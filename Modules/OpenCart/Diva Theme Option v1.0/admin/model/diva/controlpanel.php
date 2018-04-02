9d77dcd22a56e2760a1d03534e3ceabd4851d5c7
kazu7391 <vinhtq.0703@gmail.com>
2018-04-02 16:22:12 +0700

    Theme Option

--- Modules/OpenCart/Diva Theme Option v1.0/admin/model/diva/controlpanel.php
+++ Modules/OpenCart/Diva Theme Option v1.0/admin/model/diva/controlpanel.php
@@ -7,6 +7,7 @@ class ModelDivaControlpanel extends Model
         $this->setupTestimonialData();
         $this->setupMenuData();
         $this->setupFeaturedCategories();
+        $this->setupNewsletter();
         $this->setupRotateImage();
         $this->setupColorSwatches();
     }
@@ -208,6 +209,20 @@ class ModelDivaControlpanel extends Model
 
         $this->load->model('diva/featuredcate');
         $this->model_diva_featuredcate->createFeaturedCate();
+    }
+
+    public function setupNewsletter() {
+        $this->db->query("
+			CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "dvnewsletter_email` (
+			    `newsletter_id` INT(11) NOT NULL AUTO_INCREMENT,
+			    `subscribe` TINYINT(1) NOT NULL DEFAULT '1',
+	            `mail` varchar(255) NOT NULL,
+	        PRIMARY KEY (`newsletter_id`)
+		) DEFAULT COLLATE=utf8_general_ci;");
+
+        $this->load->model('user/user_group');
+        $this->model_user_user_group->addPermission($this->user->getGroupId(), 'access', 'diva/newsletter');
+        $this->model_user_user_group->addPermission($this->user->getGroupId(), 'modify', 'diva/newsletter');
     }
 
     public function setupRotateImage() {
