f766764b6952673e08b3f346c43d69deca23e941
kazu7391 <vinhtq.0703@gmail.com>
2018-04-09 16:13:43 +0700

    Most Viewed

---
+++ Modules/OpenCart/Diva Most Viewed Products/catalog/language/en-gb/diva/module/dvmostviewed.php
@@ -0,0 +1,5 @@
+<?php
+$_['title'] = "Most Viewed Products";
+
+$_['text_sale'] = "Sale";
+$_['text_new'] = "New";
---
+++ Modules/OpenCart/Diva Most Viewed Products/catalog/model/diva/product.php
@@ -0,0 +1,38 @@
+<?php
+class ModelDivaProduct extends Model
+{
+    public function getMostViewed($limit) {
+        $product_data = array();
+
+        $query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.viewed DESC LIMIT " . (int)$limit);
+
+        $this->load->model('catalog/product');
+
+        foreach ($query->rows as $result) {
+            $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
+        }
+
+        return $product_data;
+    }
+
+    public function getRandom($limit) {
+        $sql = "SELECT *, p.product_id, (SELECT AVG(rating) AS total FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = p.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "'";
+
+        $sql .= " GROUP BY p.product_id";
+
+        $sql .= " ORDER BY Rand()";
+
+        $sql .= " LIMIT " . (int) $limit;
+
+        $product_data = array();
+        $query = $this->db->query($sql);
+
+        $this->load->model('catalog/product');
+        
+        foreach ($query->rows as $result) {
+            $product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
+        }
+
+        return $product_data;
+    }
+}
