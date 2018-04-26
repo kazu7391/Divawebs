191709299ee20c0ad33ad5456955c9532235ce8f
kazu7391 <vinhtq.0703@gmail.com>
2018-04-26 21:22:27 +0700

    Tab Pro

---
+++ Modules/OpenCart/Diva Product Tabs/admin/language/en-gb/extension/module/dvtabproduct.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']     = '<b style="color: #eb5202;"><i><i class="fa fa-shopping-bag"></i> DIVA Product Tabs</i></b>';
+$_['page_title']        = 'DIVA Product Tabs';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified this module!';
+$_['text_edit']         = 'Edit DIVA Product Tabs Module';
+$_['text_bestseller']   = 'Bestseller';
+$_['text_mostviewed']   = 'Most Viewed';
+$_['text_random']       = 'Random';
+$_['text_special']      = 'Special';
+$_['text_latest']       = 'Latest';
+
+// Entry
+$_['entry_name']        = 'Module Name';
+$_['entry_status']      = 'Status';
+$_['entry_type']        = 'Type';
+$_['entry_width']       = 'Width';
+$_['entry_height']      = 'Height';
+$_['entry_description'] = 'Description';
+$_['entry_items']       = 'Items';
+$_['entry_limit']       = 'Limit';
+$_['entry_speed']       = 'Speed';
+$_['entry_auto']        = 'Auto';
+$_['entry_rows']        = 'Rows';
+$_['entry_pagination']  = 'Pagination';
+$_['entry_navigation']  = 'Navigation';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify DIVA Product Tabs module!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']       = 'Width required!';
+$_['error_height']      = 'Height required!';
