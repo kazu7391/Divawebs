ebb308f49534e559a5787f3d9db4a7f8950557dd
kazu7391 <vinhtq.0703@gmail.com>
2018-05-03 00:39:52 +0700

    Change text

--- Modules/OpenCart/Diva Blog/admin/language/en-gb/diva/blog/list.php
+++ Modules/OpenCart/Diva Blog/admin/language/en-gb/diva/blog/list.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']          = '<b style="color: #eb5202;"><i>Articles List</i></b>';
-$_['page_title']             = 'Articles List';
+$_['heading_title']          = '<b style="color: #eb5202;"><i>Blog Categories</i></b>';
+$_['page_title']             = 'Blog Categories';
 
 // Text
-$_['text_success']           = 'Success: You have modified articles list!';
-$_['text_list']              = 'Articles Lists';
-$_['text_add']               = 'Add Article List';
-$_['text_edit']              = 'Edit Article List';
+$_['text_success']           = 'Success: You have modified Blog Categories';
+$_['text_list']              = 'Blog Categories';
+$_['text_add']               = 'Add Category';
+$_['text_edit']              = 'Edit Category';
 $_['text_extension']         = 'Extensions';
 $_['text_blog']              = 'Blog';
 
 // Column
-$_['column_name']            = 'List Name';
+$_['column_name']            = 'Name';
 $_['column_status']          = 'Status';
 $_['column_action']          = 'Action';
 
 // Entry
-$_['entry_name']             = 'List Name';
+$_['entry_name']             = 'Name';
 $_['entry_post_list']        = 'Posts';
 $_['entry_status']           = 'Status';
 
 // Error
 $_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']       = 'Warning: You do not have permission to modify articles!';
+$_['error_permission']       = 'Warning: You do not have permission to modify Blog Categories!';
 $_['error_name']             = 'Article Name must be greater than 3 and less than 255 characters!';
 $_['error_meta_title']       = 'Meta Title must be greater than 3 and less than 255 characters!';
 $_['error_keyword']          = 'SEO keyword already in use!';
