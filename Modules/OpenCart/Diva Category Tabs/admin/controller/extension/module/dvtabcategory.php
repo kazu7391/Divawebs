3d00723384134303266f1fe399a3d525cab4b7da
kazu7391 <vinhtq.0703@gmail.com>
2018-05-02 18:24:55 +0700

    Category Tab

---
+++ Modules/OpenCart/Diva Category Tabs/admin/controller/extension/module/dvtabcategory.php
@@ -0,0 +1,248 @@
+<?php
+class ControllerExtensionModuleDvtabcategory extends Controller
+{
+    private $error = array();
+
+    public function index() {
+        $this->load->language('extension/module/dvtabcategory');
+
+        $this->document->setTitle($this->language->get('page_title'));
+
+        $this->load->model('setting/module');
+
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            if (!isset($this->request->get['module_id'])) {
+                $this->model_setting_module->addModule('dvtabcategory', $this->request->post);
+            } else {
+                $this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
+            }
+
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            $this->response->redirect($this->url->link('diva/module', 'user_token=' . $this->session->data['user_token'], true));
+        }
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        if (isset($this->error['name'])) {
+            $data['error_name'] = $this->error['name'];
+        } else {
+            $data['error_name'] = '';
+        }
+
+        if (isset($this->error['width'])) {
+            $data['error_width'] = $this->error['width'];
+        } else {
+            $data['error_width'] = '';
+        }
+
+        if (isset($this->error['height'])) {
+            $data['error_height'] = $this->error['height'];
+        } else {
+            $data['error_height'] = '';
+        }
+
+        $data['breadcrumbs'] = array();
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_home'),
+            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+        );
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_extension'),
+            'href' => $this->url->link('diva/module', 'user_token=' . $this->session->data['user_token'], true)
+        );
+
+        if (!isset($this->request->get['module_id'])) {
+            $data['breadcrumbs'][] = array(
+                'text' => $this->language->get('heading_title'),
+                'href' => $this->url->link('extension/module/dvtabcategory', 'user_token=' . $this->session->data['user_token'], true)
+            );
+        } else {
+            $data['breadcrumbs'][] = array(
+                'text' => $this->language->get('heading_title'),
+                'href' => $this->url->link('extension/module/dvtabcategory', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true)
+            );
+        }
+
+        if (!isset($this->request->get['module_id'])) {
+            $data['action'] = $this->url->link('extension/module/dvtabcategory', 'user_token=' . $this->session->data['user_token'], true);
+        } else {
+            $data['action'] = $this->url->link('extension/module/dvtabcategory', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
+        }
+
+        $data['cancel'] = $this->url->link('diva/module', 'user_token=' . $this->session->data['user_token'], true);
+
+        if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+            $module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
+        }
+
+        if (isset($this->request->post['name'])) {
+            $data['name'] = $this->request->post['name'];
+        } elseif (!empty($module_info)) {
+            $data['name'] = $module_info['name'];
+        } else {
+            $data['name'] = '';
+        }
+
+        if (isset($this->request->post['status'])) {
+            $data['status'] = $this->request->post['status'];
+        } elseif (!empty($module_info)) {
+            $data['status'] = $module_info['status'];
+        } else {
+            $data['status'] = 1;
+        }
+
+        if (isset($this->request->post['width'])) {
+            $data['width'] = $this->request->post['width'];
+        } elseif (!empty($module_info)) {
+            $data['width'] = $module_info['width'];
+        } else {
+            $data['width'] = 200;
+        }
+
+        if (isset($this->request->post['height'])) {
+            $data['height'] = $this->request->post['height'];
+        } elseif (!empty($module_info)) {
+            $data['height'] = $module_info['height'];
+        } else {
+            $data['height'] = 200;
+        }
+
+        if (isset($this->request->post['description'])) {
+            $data['description'] = $this->request->post['description'];
+        } elseif (!empty($module_info)) {
+            $data['description'] = $module_info['description'];
+        } else {
+            $data['description'] = 1;
+        }
+
+        if (isset($this->request->post['rows'])) {
+            $data['rows'] = $this->request->post['rows'];
+        } elseif (!empty($module_info)) {
+            $data['rows'] = $module_info['rows'];
+        } else {
+            $data['rows'] = 1;
+        }
+
+        if (isset($this->request->post['limit'])) {
+            $data['limit'] = $this->request->post['limit'];
+        } elseif (!empty($module_info)) {
+            $data['limit'] = $module_info['limit'];
+        } else {
+            $data['limit'] = 10;
+        }
+
+        if (isset($this->request->post['items'])) {
+            $data['items'] = $this->request->post['items'];
+        } elseif (!empty($module_info)) {
+            $data['items'] = $module_info['items'];
+        } else {
+            $data['items'] = 4;
+        }
+
+        if (isset($this->request->post['auto'])) {
+            $data['auto'] = $this->request->post['auto'];
+        } elseif (!empty($module_info)) {
+            $data['auto'] = $module_info['auto'];
+        } else {
+            $data['auto'] = 1;
+        }
+
+        if (isset($this->request->post['speed'])) {
+            $data['speed'] = $this->request->post['speed'];
+        } elseif (!empty($module_info)) {
+            $data['speed'] = $module_info['speed'];
+        } else {
+            $data['speed'] = 500;
+        }
+
+        if (isset($this->request->post['navigation'])) {
+            $data['navigation'] = $this->request->post['navigation'];
+        } elseif (!empty($module_info)) {
+            $data['navigation'] = $module_info['navigation'];
+        } else {
+            $data['navigation'] = 1;
+        }
+
+        if (isset($this->request->post['pagination'])) {
+            $data['pagination'] = $this->request->post['pagination'];
+        } elseif (!empty($module_info)) {
+            $data['pagination'] = $module_info['pagination'];
+        } else {
+            $data['pagination'] = 0;
+        }
+
+        if (isset($this->request->post['second_image'])) {
+            $data['second_image'] = $this->request->post['second_image'];
+        } elseif (!empty($module_info)) {
+            $data['second_image'] = $module_info['second_image'];
+        } else {
+            $data['second_image'] = 0;
+        }
+
+        $data['user_token'] = $this->session->data['user_token'];
+
+        $this->load->model('catalog/category');
+
+        if (isset($this->request->post['product_category'])) {
+            $categories = $this->request->post['product_category'];
+        } elseif (!empty($module_info)) {
+            $categories = $module_info['product_category'];
+        } else {
+            $categories = array();
+        }
+
+        $data['product_categories'] = array();
+
+        foreach ($categories as $category_id) {
+            $category_info = $this->model_catalog_category->getCategory($category_id);
+
+            if ($category_info) {
+                $data['product_categories'][] = array(
+                    'category_id' => $category_info['category_id'],
+                    'name' => ($category_info['path']) ? $category_info['path'] . ' &gt; ' . $category_info['name'] : $category_info['name']
+                );
+            }
+        }
+
+        $this->document->addStyle('view/stylesheet/divawebs/themeadmin.css');
+        $this->document->addScript('view/javascript/divawebs/switch-toggle/js/bootstrap-toggle.min.js');
+        $this->document->addStyle('view/javascript/divawebs/switch-toggle/css/bootstrap-toggle.min.css');
+
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['header'] = $this->load->controller('common/header');
+        $data['footer'] = $this->load->controller('common/footer');
+
+        $this->response->setOutput($this->load->view('diva/module/dvtabcategory', $data));
+    }
+
+    private function validate() {
+        if (!$this->user->hasPermission('modify', 'extension/module/dvtabcategory')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
+
+        if (!$this->request->post['width']) {
+            $this->error['width'] = $this->language->get('error_width');
+        }
+
+        if (!$this->request->post['height']) {
+            $this->error['height'] = $this->language->get('error_height');
+        }
+
+        if ($this->error && !isset($this->error['warning'])) {
+            $this->error['warning'] = $this->language->get('error_warning');
+        }
+
+        if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+            $this->error['name'] = $this->language->get('error_name');
+        }
+
+        return !$this->error;
+    }
+}
---
+++ Modules/OpenCart/Diva Category Tabs/catalog/language/en-gb/diva/module/dvtabcategory.php
@@ -0,0 +1,7 @@
+<?php
+$_['title']     = "Categories Tabs";
+
+// Text
+$_['text_tax']  = 'Ex Tax:';
+$_['text_sale'] = "Sale";
+$_['text_new']  = "New";
